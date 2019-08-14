a2c649a43955d62c40977e3a05384b7c46a870d0
Tristan Robert <tristan.robert.44@gmail.com>
2019-08-14 18:04:39 +0200

    Update backup

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+source backup.env
 lftp -u $FTP_USER,$FTP_PASSWORD -e "mirror --delete --only-newer --verbose $SRC $DEST --log=$BACKUP_LOG_FILE" $FTP_SITE &
--- backup/restore.sh
+++ backup/restore.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+source backup.env
 lftp -u $FTP_USER,$FTP_PASSWORD -e "mirror --reverse --delete --only-newer --verbose $SRC $DEST --log=$RESTORE_LOG_FILE" $FTP_SITE &
