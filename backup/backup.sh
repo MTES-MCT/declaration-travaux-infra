3bac0fbd2e300b1b79947d892d86340e3d3ee0d1
Tristan Robert <tristan.robert.44@gmail.com>
2019-08-19 12:48:49 +0200

    Fix backup shell

--- backup/backup.sh
+++ backup/backup.sh
@@ -7,12 +7,10 @@ source backup.env
 TIME=`date +%d-%m-%Y"_"%H_%M_%S`
 FILENAME=backup-rieau-$TIME.tar.gz
 
-docker-compose -f ../app/docker-compose.yml exec -T db pg_dump -U rieau > rieau_dump_$TIME.sql
-docker-compose -f ../sso/docker-compose.yml exec -T db pg_dump -U keycloak > sso_dump_$TIME.sql
+docker-compose -f ../app/docker-compose.yml exec -T db pg_dump -U rieau > rieau_dump.sql
+docker-compose -f ../sso/docker-compose.yml exec -T db pg_dump -U keycloak > sso_dump.sql
 
-rm *_dump_*.sql
-
-tar -zcvf $DEST/$FILENAME $SRC/traefik/acme.json *_dump_*.sql
+tar -zcvf $DEST/$FILENAME $SRC/traefik/acme.json *_dump.sql
 
 ftp -p -inv $FTP_SITE <<EOF
     user $FTP_USER $FTP_PASS
@@ -20,5 +18,7 @@ ftp -p -inv $FTP_SITE <<EOF
     mput *.tar.gz
     bye
 EOF
+
+rm *_dump_*.sql
 
 rm $DEST/$FILENAME
