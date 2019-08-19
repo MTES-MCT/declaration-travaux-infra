ffd1e928bb2d6f42269a273e0eadb932118e60a8
Tristan Robert <tristan.robert.44@gmail.com>
2019-08-19 12:45:50 +0200

    Update backup shell

--- backup/backup.sh
+++ backup/backup.sh
@@ -10,6 +10,8 @@ FILENAME=backup-rieau-$TIME.tar.gz
 docker-compose -f ../app/docker-compose.yml exec -T db pg_dump -U rieau > rieau_dump_$TIME.sql
 docker-compose -f ../sso/docker-compose.yml exec -T db pg_dump -U keycloak > sso_dump_$TIME.sql
 
+rm *_dump_*.sql
+
 tar -zcvf $DEST/$FILENAME $SRC/traefik/acme.json *_dump_*.sql
 
 ftp -p -inv $FTP_SITE <<EOF
