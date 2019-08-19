c800cad31a4ddf62ba59fd30f5dc7c7a2a0538cf
Tristan Robert <tristan.robert.44@gmail.com>
2019-08-19 12:44:33 +0200

    Fix backup shell

--- backup/backup.sh
+++ backup/backup.sh
@@ -8,7 +8,7 @@ TIME=`date +%d-%m-%Y"_"%H_%M_%S`
 FILENAME=backup-rieau-$TIME.tar.gz
 
 docker-compose -f ../app/docker-compose.yml exec -T db pg_dump -U rieau > rieau_dump_$TIME.sql
-docker-compose -f ../sso/docker-compse.yml exec -T db pg_dump -U keycloak > sso_dump_$TIME.sql
+docker-compose -f ../sso/docker-compose.yml exec -T db pg_dump -U keycloak > sso_dump_$TIME.sql
 
 tar -zcvf $DEST/$FILENAME $SRC/traefik/acme.json *_dump_*.sql
 
